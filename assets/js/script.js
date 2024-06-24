40c329463b9df5abe6fc56977185dae33f534f5c
Hasibullah Fathi <hasibullah.fathi@gmail.com>
2024-06-24 19:03:25 +0000

    add mutiplication to the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,8 +21,10 @@ function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
-    if(gameType === "addition") {
+    if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multyply") {
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting!`;
@@ -53,6 +55,8 @@ function calculateCorrectAnswer() {
 
     if(operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multyply"];
     } else {
         alert(`unknown operator: ${operator}`);
         throw `unknown operator: ${operator}. Aborting!`;
@@ -80,7 +84,9 @@ function displaySubtractQuestion() {
     
 }
 
-function displayMultiplyQuestion() {
-    
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
 
